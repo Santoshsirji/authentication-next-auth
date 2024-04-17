0a99580c38f3d57726cc9a271723121270eab202
Santosh Raut <santoshraut9862281653@gmail.com>
2024-04-17 17:37:22 +0545

    verificatin token creation

--- actions/login.ts
+++ actions/login.ts
@@ -6,6 +6,8 @@ import { AuthError } from "next-auth";
 import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -16,6 +18,18 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     }
 
     const { email, password } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if(!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist"}
+    }
+
+    if(!existingUser.emailVerified) {
+         const verificationToken = await generateVerificationToken(email);
+         return { success: "Confirmation email sent!"};
+    }
+
 
     try{
        await signIn("credentials", {
--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs"
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -31,6 +32,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         }
     });
 
+    const verificationToken = await generateVerificationToken(email);
+
+
     // Todo : Send verification Token email
-    return { success: "User Created!"}
+    return { success: "Confirmation Email Sent!"}
 }
